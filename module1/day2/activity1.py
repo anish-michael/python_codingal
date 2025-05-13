da2b0173bf5f7c0630d801617a94a3f49fbf2b78
anish-michael <anishmichaelvs@gmail.com>
2025-05-13 18:42:02 +0530

    added activity 1 day2

---
+++ module1/day2/activity1.py
@@ -0,0 +1,10 @@
+# print('hello anish')
+
+name = input('Enter your name: ')
+print(f'hello {name}')
+
+age = int(input('what is your age: '))
+if age >= 9 : 
+  print(f'{age} years old means you are in class 4 to office ')
+else :
+  print('then your in class LKG to 4 std')
